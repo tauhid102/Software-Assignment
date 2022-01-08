fcdbbe0b11fe0a1f6ac552480587ceb515c64bc6
Md. Rizwan Chowdhury <48764917+mrizwanc@users.noreply.github.com>
2022-01-08 23:10:31 +0600

    y
    implementing  Singleton Pattern

--- MyFancyJavaProject/src/MyClass.java
+++ MyFancyJavaProject/src/MyClass.java
@@ -1,8 +1,41 @@
+class Database {
+   private static Database dbObject;
+
+   private Database() {      
+   }
+
+   public static Database getInstance() {
+
+      // create object if it's not already created
+      if(dbObject == null) {
+         dbObject = new Database();
+      }
+
+       // returns the singleton object
+       return dbObject;
+   }
+
+   public void getConnection() {
+       System.out.println("You are now connected to the database.");
+   }
+}
+
+
+
+
 
 public class MyClass {
     public static void main(String[] args) {
-        int a;
-        a=10;
-        System.out.println("A is: "+a);
+       Database db1;
+
+      // refers to the only object of Database
+      db1= Database.getInstance();
+      
+      db1.getConnection();
+	   
+	   
+	   
+	   
+	   
     }
 }
